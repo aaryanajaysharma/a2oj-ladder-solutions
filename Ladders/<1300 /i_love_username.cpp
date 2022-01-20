b41943a88d70ce218fb68b640104aa4423cd8461
aaryanajaysharma <202051001@iiitvadodara.ac.in>
2022-01-21 00:59:22 +0530

    added files

--- Ladders/<1300 /i_love_username.cpp
+++ Ladders/<1300 /i_love_username.cpp
@@ -10,15 +10,28 @@
 #include<math.h>
 #include<stdlib.h>
 #include<deque>
+#include<limits.h>
 using namespace std;
 
 int main()
 {
-    int t = 1;
-    cin>>t;
-    while(t--)
+    int n;
+    cin >> n;
+    vector<int> a(n);
+    int min = INT_MAX, max = INT_MIN;
+    int amazing = 0;
+    for(int i = 0; i < n; i++)
     {
-         
+        cin >> a[i];
+        if(a[i] < min){
+            min = a[i];
+            amazing++;
+        }
+        if(a[i] > max){
+            max = a[i];
+            amazing++;
+        }
     }
+    cout << amazing - 2 << endl;
     return 0;
 }
--- Ladders/<1300 /lightsout.cpp
+++ Ladders/<1300 /lightsout.cpp
@@ -1,110 +1,64 @@
-#include<iostream>
-#include<algorithm>
-#include<string>
-#include<vector>
-#include<set>
-#include<unordered_set>
-#include<map>
-#include<unordered_map>
-#include<queue>
-#include<math.h>
-#include<stdlib.h>
-#include<deque>
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <vector>
+#include <set>
+#include <unordered_set>
+#include <map>
+#include <unordered_map>
+#include <queue>
+#include <math.h>
+#include <stdlib.h>
+#include <deque>
 using namespace std;
-
-void toggle(int a[3][3], int i, int j)
-{
-    if(i == 0 && j == 0){
-        a[i][j]  = a[i][j] == 0 ? 1 : 0;
-        a[i][j+1] = a[i][j+1] == 0 ? 1 : 0;
-        a[i+1][j] = a[i+1][j] == 0 ? 1 : 0;
-    }
-    if(i == 2 && j == 2){
-        a[i][j]  = a[i][j] == 0 ? 1 : 0;
-        a[i][j-1] = a[i][j-1] == 0 ? 1 : 0;
-        a[i-1][j] = a[i-1][j] == 0 ? 1 : 0;
-    }
-    if(i == 0 && j == 2){
-        a[i][j]  = a[i][j] == 0 ? 1 : 0;
-        a[i+1][j] = a[i+1][j] == 0 ? 1 : 0;
-        a[i][j-1] = a[i][j-1] == 0 ? 1 : 0;
-    }
-    if(i == 2 && j == 0){
-        a[i][j]  = a[i][j] == 0 ? 1 : 0;
-        a[i-1][j] = a[i-1][j] == 0 ? 1 : 0;
-        a[i][j+1] = a[i][j+1] == 0 ? 1 : 0;
-    }
-    if(i == 1 && j == 1){
-        a[i][j]  = a[i][j] == 0 ? 1 : 0;
-        a[i-1][j] = a[i-1][j] == 0 ? 1 : 0;
-        a[i+1][j] = a[i+1][j] == 0 ? 1 : 0;
-        a[i][j-1] = a[i][j-1] == 0 ? 1 : 0;
-        a[i][j+1] = a[i][j+1] == 0 ? 1 : 0;
-    }
-
-    if(i == 0 && j == 1){
-        a[i][j]  = a[i][j] == 0 ? 1 : 0;
-        a[i+1][j] = a[i+1][j] == 0 ? 1 : 0;
-        a[i][j-1] = a[i][j-1] == 0 ? 1 : 0;
-        a[i][j+1] = a[i][j+1] == 0 ? 1 : 0;
-    }
-
-    if(i == 1 && j == 0){
-        a[i][j]  = a[i][j] == 0 ? 1 : 0;
-        a[i-1][j] = a[i-1][j] == 0 ? 1 : 0;
-        a[i+1][j] = a[i+1][j] == 0 ? 1 : 0;
-        a[i][j+1] = a[i][j+1] == 0 ? 1 : 0;
-    }
-
-    if(i == 1 && j == 2){
-        a[i][j]  = a[i][j] == 0 ? 1 : 0;
-        a[i-1][j] = a[i-1][j] == 0 ? 1 : 0;
-        a[i+1][j] = a[i+1][j] == 0 ? 1 : 0;
-        a[i][j-1] = a[i][j-1] == 0 ? 1 : 0;
-    }
-
-    if(i == 2 && j == 1){
-        a[i][j]  = a[i][j] == 0 ? 1 : 0;
-        a[i-1][j] = a[i-1][j] == 0 ? 1 : 0;
-        a[i][j-1] = a[i][j-1] == 0 ? 1 : 0;
-        a[i][j+1] = a[i][j+1] == 0 ? 1 : 0;
-    }
-}
 int main()
 {
-    int a[3][3];
-    int ans[3][3] = {
-        {1,1,1},
-        {1,1,1},
-        {1,1,1}
-    };
+    int a[3][3], b[3][3];
     for (int i = 0; i < 3; i++)
     {
         for (int j = 0; j < 3; j++)
         {
             cin >> a[i][j];
-            a[i][j] = a[i][j] % 2;
+            b[i][j] = 1;
         }
     }
     for (int i = 0; i < 3; i++)
     {
         for (int j = 0; j < 3; j++)
         {
-            while(a[i][j]--)
+            if (a[i][j] == 0 || a[i][j] % 2 == 0)
             {
-                toggle(ans, i, j);
+                continue;
+            }
+            else
+            {
+                b[i][j] = 1 - b[i][j];
+                if ((j + 1) < 3)
+                {
+                    b[i][j + 1] = 1 - b[i][j + 1];
+                }
+                if ((i + 1) < 3)
+                {
+                    b[i + 1][j] = 1 - b[i + 1][j];
+                }
+                if ((i - 1) >= 0)
+                {
+                    b[i - 1][j] = 1 - b[i - 1][j];
+                }
+                if ((j - 1) >= 0)
+                {
+                    b[i][j - 1] = 1 - b[i][j - 1];
+                }
             }
         }
     }
-
     for (int i = 0; i < 3; i++)
     {
         for (int j = 0; j < 3; j++)
         {
-            cout << ans[i][j];
+            cout << b[i][j];
         }
-        cout << endl;
+        cout << "\n";
     }
-
     return 0;
 }
