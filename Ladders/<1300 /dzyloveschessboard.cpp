f3731daaefefa4d2ab6e50d17e1e5f115ce95009
aaryanajaysharma <202051001@iiitvadodara.ac.in>
2022-02-18 23:54:33 +0530

    new solutions added

---
+++ Ladders/<1300 /dzyloveschessboard.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <vector>
+#include <set>
+#include <unordered_set>
+#include <map>
+#include <unordered_map>
+#include <queue>
+#include <math.h>
+#include <stdlib.h>
+#include <deque>
+using namespace std;
+
+int main()
+{
+    int n, m;
+    cin >> n >> m;
+    char a[n][m];
+    char solved[n][m];
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            cin >> a[i][j];
+            (i + j) % 2 == 0 ? solved[i][j] = 'B' : solved[i][j] = 'W';
+            if (a[i][j] == '.')
+            {
+                a[i][j] = solved[i][j];
+            }
+        }
+    }
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            cout << a[i][j];
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
---
+++ Ladders/<1300 /pashmakandflowers.cpp
@@ -0,0 +1,49 @@
+#include<iostream>
+#include<algorithm>
+#include<string>
+#include<vector>
+#include<set>
+#include<unordered_set>
+#include<map>
+#include<unordered_map>
+#include<queue>
+#include<math.h>
+#include<stdlib.h>
+#include<deque>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    int* a = new int[n];
+    int m = INT32_MAX, M = INT32_MIN;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+        if(a[i] > M)
+        {
+            M = a[i];
+        }
+        if(a[i] < m)
+        {
+            m = a[i];
+        }
+    }
+    int m_count = 0, M_count = 0;
+    for (int i = 0; i < n; i++)
+    {
+        if(a[i] == m)
+        {
+            m_count++;
+        }
+        if(a[i] == M)
+        {
+            M_count++;
+        }
+    }
+    long long product;
+    m != M? product =  ((long long) m_count)*((long long) M_count): product = ((long long) m_count)*((long long) (m_count - 1))/2;
+    cout << M - m <<" " << product << endl;
+    return 0;
+}
